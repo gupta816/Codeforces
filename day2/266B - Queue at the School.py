13852c1bdf1f8ad0cb98ebd47a672daf6bd56061
gupta816 <46747698+gupta816@users.noreply.github.com>
2020-10-02 18:01:37 +0530

    Create 266B - Queue at the School.py

---
+++ day2/266B - Queue at the School.py
@@ -0,0 +1,15 @@
+n,t=map(int,input().split())
+st=input()
+s=list(st)
+while(t!=0):
+    i=0
+    while(i<n-1):
+        if s[i]=="B" and s[i+1]=="G":
+            s[i],s[i+1]=s[i+1],s[i]
+            i+=2
+        else:
+            i+=1
+            
+        
+    t-=1
+print("".join(s))
